e8c82ba828f295d3c9687e322ef1d71f86e6eda0
evozkio <39371612+evozkio@users.noreply.github.com>
2022-03-16 14:10:19 +0100

    Primer dia

--- practica-1/src/app/app.component.ts
+++ practica-1/src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'practica-1';
+  @ViewChild('data') inputData: any; // accessing the reference element
+  handleClear(){
+     this.inputData.nativeElement.value = ' ';
+  }
+  public muestraConsola(variable:any):void{
+    console.log(variable);
+  }
 }
+
+
+
