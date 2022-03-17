9a27b57b3eff510cc61ba65ae5f374f62a8ebe96
evozkio <39371612+evozkio@users.noreply.github.com>
2022-03-17 09:16:03 +0100

    pruebas calendario

--- practica-1/src/app/app.component.ts
+++ practica-1/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
+import {NgbDate, NgbCalendar, NgbDateParserFormatter} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-root',
@@ -7,14 +8,28 @@ import { Component, ElementRef, ViewChild } from '@angular/core';
 })
 export class AppComponent {
   title = 'practica-1';
-  @ViewChild('data') inputData: any; // accessing the reference element
+  @ViewChild('data') inputData: any; 
   handleClear(){
      this.inputData.nativeElement.value = ' ';
   }
   public muestraConsola(variable:any):void{
     console.log(variable);
   }
+@Component({
+  selector: 'ngbd-datepicker-range-popup',
+  templateUrl: './datepicker-range-popup.html',
+  styles: [`
+    .dp-hidden {
+      width: 0;
+      margin: 0;
+      border: none;
+      padding: 0;
+    }
+    .custom-day {
+      text-align: center;
+      padding: 0.185rem 0.25rem;
+      display: inline-block;
+      height: 2rem;
+      width: 2rem;
+    })
 }
-
-
-
